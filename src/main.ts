25a1316c48200660bc545e28121f3f4c617fea5e
edsanagomes <edsanagomes@gmail.com>
2025-12-15 10:38:54 +0100

    fix : update format

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import './style.css'
 
-const button = document.querySelector<HTMLButtonElement>('#add-todo-button') /* c'est comme je suis sûre qu'il y de HTMLButtonElement*/
+const button =
+  document.querySelector<HTMLButtonElement>(
+    '#add-todo-button',
+  ) /* c'est comme je suis sûre qu'il y de HTMLButtonElement*/
 const list = document.querySelector<HTMLUListElement>('#todo-elements')
 const input = document.querySelector<HTMLInputElement>('#todo-input')
 
-if (!input || !button || !list) { /* ! - si les elements n'existe pas */
-  throw new Error("Attention !!! Input n'a pas été trouvé sur la page") /* je peux aussi utiliser console.logo avant avec message - protege*/
+if (!input || !button || !list) {
+  /* ! - si les elements n'existe pas */
+  throw new Error(
+    "Attention !!! Input n'a pas été trouvé sur la page",
+  ) /* je peux aussi utiliser console.logo avant avec message - protege*/
 }
 
 const error = document.createElement('div')
@@ -14,7 +20,8 @@ error.style.color = 'red'
 error.style.display = 'none'
 input.insertAdjacentElement('beforebegin', error)
 
-const addTodo = () => { /* rend la valeur du contexte où la fonction a été créée */
+const addTodo = () => {
+  /* rend la valeur du contexte où la fonction a été créée */
   const value = input.value.trim()
   if (value === '') {
     error.style.display = 'block'
