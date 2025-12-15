10508e05dfb7c715877aba1357857b748ea15941
edsanagomes <edsanagomes@gmail.com>
2025-12-15 13:09:59 +0100

    fix: update ts checks

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,24 @@
 import './style.css'
-
 const button =
   document.querySelector<HTMLButtonElement>(
     '#add-todo-button',
   ) /* c'est comme je suis sûre qu'il y de HTMLButtonElement*/
 const list = document.querySelector<HTMLUListElement>('#todo-elements')
 const input = document.querySelector<HTMLInputElement>('#todo-input')
+const error = document.querySelector<HTMLDivElement>('#error')
 
-if (!input || !button || !list) {
+if (!input || !button || !list || !error) {
   /* ! - si les elements n'existe pas */
   throw new Error(
     "Attention !!! Input n'a pas été trouvé sur la page",
   ) /* je peux aussi utiliser console.logo avant avec message - protege*/
 }
 
-const error = document.createElement('div')
-error.textContent = 'Veuillez entrer une tâche.'
-error.style.color = 'red'
-error.style.display = 'none'
-input.insertAdjacentElement('beforebegin', error)
-
 const addTodo = () => {
   /* rend la valeur du contexte où la fonction a été créée */
   const value = input.value.trim()
   if (value === '') {
+    error.textContent = 'Veuillez entrer une tâche.'
     error.style.display = 'block'
     return
   }
